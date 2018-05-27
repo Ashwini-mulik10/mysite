beadc7a895ad63ee381b4b285c5d3e881ca192f0
ashwini mulik <ashwinimulik10@gmail.com>
2018-05-27 13:30:53 +0530

    first commit

---
+++ polls/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+class Comment(models.Model):
+	name=models.CharField(max_length=20)
+	comment=models.TextField()
+	date_added=models.DateTimeField(default=timezone.now)
+
+	def __str__(self):
+    		return '<name: {},ID: {}>'.format(self.name,self.id)
